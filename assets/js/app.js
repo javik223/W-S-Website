026f6ae8980c563bb101cb50a638cc4976d970f5
Victory James <javik@live.com>
2014-11-06 23:29:19 +0100

    work and about pages done

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,17 +19,31 @@
         s.set(heroContainer, {width: wW, height: wH - featuredHH - headerH});
     }
 
-    var ps = new ParticleSlider({
-        sliderId: 'particle-slider-2', //id of HTML-Element
-        slideDelay: 6.0, //delay between slides
-        ptlSize: 2,
-        velocityX: 200,
-        velocityY: 200,
-        ptlGap: 1,
-    });
+    if ($("#particle-slider-2").length > 0 ) {
+        var ps = new ParticleSlider({
+            sliderId: 'particle-slider-2', //id of HTML-Element
+            slideDelay: 6.0, //delay between slides
+            ptlSize: 2,
+            velocityX: 200,
+            velocityY: 200,
+            ptlGap: 1,
+        });
 
-    ps.gravityX = ps.width - 1;
+        ps.gravityX = ps.width - 1;
+
+    }
 
      $(window).on('resize', resizeHeroFeature);
+
+     var works_container = document.body.querySelector('.works_collection');
+     console.log(works_container);
+
+     var pckry = new Packery( works_container, {
+        itemSelector: '.item',
+        gutter: 0,
+        "isOriginTop": true,
+        "rowHeight": 150,
+        isResizeBound: true
+     });
 
 });
